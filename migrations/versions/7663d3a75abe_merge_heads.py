332e68df4ddb7b28d0f15b70fd1f19761d6880e8
Jonathan <jonathan.dupau24@gmail.com>
2026-03-15 22:02:48 +0100

    fic heads

---
+++ migrations/versions/7663d3a75abe_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 7663d3a75abe
+Revises: 3bf03284a197, f3c9d2b1aa10
+Create Date: 2026-03-15 22:02:13.201326
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7663d3a75abe'
+down_revision = ('3bf03284a197', 'f3c9d2b1aa10')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
