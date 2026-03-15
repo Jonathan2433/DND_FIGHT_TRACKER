e7c04268332f20cf66ee0c0628eeba2c29afbaf9
Jonathan <jonathan.dupau24@gmail.com>
2026-03-15 21:04:00 +0100

    Merge Alembic heads

---
+++ migrations/versions/3bf03284a197_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge heads
+
+Revision ID: 3bf03284a197
+Revises: 4f2a1d9e8b77, c1b7e5a2d441
+Create Date: 2026-03-15 21:03:56.145129
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '3bf03284a197'
+down_revision = ('4f2a1d9e8b77', 'c1b7e5a2d441')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
